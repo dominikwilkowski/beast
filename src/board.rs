7b48fd4dc8e97b98e84bd295eab2566b63680295
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2025-02-11 06:29:35 +0100

    types

--- src/board.rs
+++ src/board.rs
@@ -1,19 +1,19 @@
-pub struct Coord {
-	x: usize,
-	y: usize,
-}
+use crate::{Coord, Tile, BOARD_HEIGHT, BOARD_WIDTH};
 
 pub struct Partial<'a> {
 	coord: Coord,
 	replacement: &'a str,
 }
 
-pub struct Board {}
+pub struct Board {
+	data: [[Tile; BOARD_WIDTH]; BOARD_HEIGHT],
+}
 
 impl Board {
 	pub fn new() -> Self {
 		Self {}
 	}
+
 	fn terrain_gen() {}
 
 	pub fn render_full() {}
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,39 @@ mod beasts;
 mod board;
 mod game;
 
+/// the board width
+pub const BOARD_WIDTH: usize = 100;
+/// the board height
+pub const BOARD_HEIGHT: usize = 40;
+/// where the player starts from
+pub const PLAYER_START: Coord = Coord { x: 1, y: 32 };
+
+/// a data structure to place items on a board
+pub struct Coord {
+	x: usize,
+	y: usize,
+}
+
+/// the items that can be found on the baord
+pub enum Tile {
+	/// empty space
+	Empty,
+	/// a block ▓▓
+	Block,
+	/// a immovable block ▓▓
+	ImmovableBlock,
+	/// the player ◄►
+	Player,
+	/// a beast ├┤
+	Beast,
+	/// a super beast ╟╢
+	SuperBeast,
+	/// a super beast egg ○○ ⚬⚬ ◦◦ ╟╢
+	Egg,
+	/// a hatched beast ╬╬
+	HatchedBeast,
+}
+
 fn main() {
 	println!("Hello, world!");
 }
