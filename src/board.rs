71ed367166d8b9ca4ccaf8438e80f1e1695ae0a1
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2025-02-14 09:18:15 +0100

    added placing beasts radially from top right

--- src/board.rs
+++ src/board.rs
@@ -1,4 +1,5 @@
-use rand::seq::IteratorRandom;
+use rand::seq::{IteratorRandom, SliceRandom};
+use std::fmt::Write;
 
 use crate::{Coord, Level, Tile, BOARD_HEIGHT, BOARD_WIDTH};
 
@@ -22,25 +23,56 @@ impl Board {
 	fn terrain_gen(level: Level) -> [[Tile; BOARD_WIDTH]; BOARD_HEIGHT] {
 		let mut data = [[Tile::Empty; BOARD_WIDTH]; BOARD_HEIGHT];
 
-		let mut rng = rand::rng();
-
-		let (blocks, immovable_blocks, _beasts) = match level {
-			Level::One => (600, 60, 3),
-			Level::Two => (400, 100, 5),
-			Level::Three => (150, 250, 12),
+		let (blocks, immovable_blocks, beasts) = match level {
+			Level::One => (300, 30, 3),
+			Level::Two => (200, 50, 5),
+			Level::Three => (50, 150, 12),
 		};
 
-		let all_coords = (0..BOARD_HEIGHT).flat_map(|y| (0..BOARD_WIDTH).map(move |x| (y, x)));
-		let total_needed = blocks + immovable_blocks;
-		let coords: Vec<(usize, usize)> = all_coords.choose_multiple(&mut rng, total_needed);
+		let mut all_positions: Vec<(usize, usize)> =
+			(0..BOARD_HEIGHT).flat_map(|y| (0..BOARD_WIDTH).map(move |x| (y, x))).collect();
 
-		for &(y, x) in coords.iter().take(blocks) {
-			data[y][x] = Tile::Block;
+		let total_blocks_needed = blocks + immovable_blocks;
+		let mut rng = rand::rng();
+		all_positions.shuffle(&mut rng);
+		let block_positions: Vec<(usize, usize)> = all_positions.drain(0..total_blocks_needed).collect();
+
+		for &(row, column) in block_positions.iter().take(blocks) {
+			data[row][column] = Tile::Block;
 		}
 
-		for &(y, x) in coords.iter().skip(blocks).take(immovable_blocks) {
-			data[y][x] = Tile::ImmovableBlock;
+		for &(row, column) in block_positions.iter().skip(blocks).take(immovable_blocks) {
+			data[row][column] = Tile::ImmovableBlock;
 		}
+
+		let top_right = (0, BOARD_WIDTH - 1);
+		all_positions.sort_by(|&(row1, column1), &(row2, column2)| {
+			let distance_row1 = row1 as isize - top_right.0 as isize;
+			let distance_column1 = column1 as isize - top_right.1 as isize;
+			let distance_row2 = row2 as isize - top_right.0 as isize;
+			let distance_column2 = column2 as isize - top_right.1 as isize;
+			// calculating the Euclidean distance
+			// distance^2 = distance_x^2+distance_y^2
+			let distance1 = distance_row1 * distance_row1 + distance_column1 * distance_column1;
+			let distance2 = distance_row2 * distance_row2 + distance_column2 * distance_column2;
+			distance1.cmp(&distance2)
+		});
+
+		let mut placed_beasts = 0;
+		let mut i = 0;
+		while placed_beasts < beasts {
+			if i >= all_positions.len() {
+				panic!("Could not find a free spot to place all beasts");
+			}
+
+			let (row, col) = all_positions[i];
+			data[row][col] = Tile::Beast;
+			placed_beasts += 1;
+			// skipping a couple tiles to give beasts some room
+			i += 16;
+		}
+
+		data[BOARD_HEIGHT - 1][0] = Tile::Player;
 
 		data
 	}
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,20 @@
+use std::fmt;
+
 mod beasts;
 mod board;
 mod game;
 
 /// the board width
-pub const BOARD_WIDTH: usize = 100;
+pub const BOARD_WIDTH: usize = 50;
 /// the board height
-pub const BOARD_HEIGHT: usize = 40;
+pub const BOARD_HEIGHT: usize = 30;
 /// where the player starts from
-pub const PLAYER_START: Coord = Coord { x: 1, y: 32 };
+pub const PLAYER_START: Coord = Coord { column: 1, row: 32 };
 
 /// a data structure to place items on a board
 pub struct Coord {
-	x: usize,
-	y: usize,
+	column: usize,
+	row: usize,
 }
 
 /// the items that can be found on the baord
@@ -20,7 +22,7 @@ pub struct Coord {
 pub enum Tile {
 	/// empty space
 	Empty,
-	/// a block ▓▓
+	/// a block ░░
 	Block,
 	/// a immovable block ▓▓
 	ImmovableBlock,
@@ -32,6 +34,8 @@ pub enum Tile {
 	SuperBeast,
 	/// a super beast egg ○○ ⚬⚬ ◦◦ ╟╢
 	Egg,
+	/// a super beast egg ○○ hatching
+	EggHatching,
 	/// a hatched beast ╬╬
 	HatchedBeast,
 }
@@ -46,5 +50,5 @@ pub enum Level {
 
 fn main() {
 	let board = crate::board::Board::new();
-	println!("{board:?}");
+	println!("{}", board.render_full());
 }
