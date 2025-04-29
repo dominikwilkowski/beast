28f9181ede8bc6a45c87c24198de4fd36728a33c
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2025-04-30 04:29:01 +1000

    fixed hash

--- beast.rb
+++ beast.rb
@@ -2,8 +2,9 @@ class Beast < Formula
   desc "An ASCII game built in rust in loving memory of the 1984 hit BEAST by Dan Baker, Alan Brown, Mark Hamilton and Derrick Shadel"
   homepage "https://github.com/dominikwilkowski/beast"
   # when updating version run `brew install --build-from-source --formula --HEAD ./beast.rb` to test locally first
+  # make sure you remove rust from brew after
   url "https://github.com/dominikwilkowski/beast/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "40abb007b829dd2e0e2cc0598a00c512ad8dd820eb31b2f9b0a8f2b89fa08d79"
+  sha256 "12b06613c4146ef77da3dac39989f9d49f56692e3991a44e7b0ac028ad4b4fdd"
   license "GPL-3.0-or-later"
   head "https://github.com/dominikwilkowski/beast.git", branch: "main"
 
